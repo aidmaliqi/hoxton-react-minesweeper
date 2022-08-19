07f8cf3a940789a1b650e890bec61a5657a7af16
Aid Maliqi <aid.maliqi04@gmail.com>
2022-08-19 16:20:29 +0200

    second commit

--- src/data/data.ts
+++ src/data/data.ts
@@ -14,77 +14,88 @@ export const boxes: box[] = [
     id: 2,
     mine: false,
     count: 0,
-  },{
+  },
+  {
     id: 3,
     mine: false,
     count: 0,
-  },{
+  },
+  {
     id: 4,
     mine: false,
     count: 0,
-  },{
+  },
+  {
     id: 5,
-    mine: true,
+    mine: false,
     count: 0,
-  },{
+  },
+  {
     id: 6,
     mine: false,
     count: 0,
-  },{
+  },
+  {
     id: 7,
-    mine: true,
+    mine: false,
     count: 0,
-  },{
+  },
+  {
     id: 8,
     mine: false,
     count: 0,
-  },{
+  },
+  {
     id: 9,
     mine: false,
     count: 0,
-  },{
+  },
+  {
     id: 10,
     mine: false,
     count: 0,
-  },{
+  },
+  {
     id: 11,
     mine: false,
     count: 0,
-  },{
+  },
+  {
     id: 12,
     mine: false,
     count: 0,
-  },{
+  },
+  {
     id: 13,
-    mine: true,
+    mine: false,
     count: 0,
-  },{
+  },
+  {
     id: 14,
     mine: false,
     count: 0,
-  },{
+  },
+  {
     id: 15,
     mine: false,
     count: 0,
-  },{
+  },
+  {
     id: 16,
     mine: false,
     count: 0,
   },
 ];
 
-
-function randomItem (items: box[]){
-
-    let item =  items[Math.floor(Math.random()*items.length)];
-    let value = 0
-    let twentyPercent  = (items.length * 0.2).toFixed(0) 
-    while (twentyPercent !== 0) {
-        
-    }
-    if (item.mine) {
-        return
-    }else{
-
-    }
+function randomItem(items: box[]) {
+  //let twentyPercent  = (items.length * 0.2).toFixed(0)
+  let twentyPercent = Math.round(items.length * 0.2);
+  while (twentyPercent !== 0) {
+    let item = items[Math.floor(Math.random() * items.length)];
+    if (item.mine === false) {
+        item.mine!
+      twentyPercent--
+    } 
+  }
 }
+randomItem(boxes)
